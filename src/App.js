a11969f9df3cad02e076b03c96db301f64fe1670
mehmetcanozturk <kaptanhiro123@gmail.com>
2022-08-16 13:03:11 +0300

    bottom text design update

--- src/App.js
+++ src/App.js
@@ -50,15 +50,15 @@ function App() {
         <div className="bottom">
           <div className="feels">
             {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
-            <p>Feels Like</p>
+            <p className='detail-text'>Feels Like</p>
           </div>
           <div className="humidity">
             {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-            <p>Humidity</p>
+            <p className='detail-text'>Humidity</p>
           </div>
           <div className="wind">
             {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-            <p>Wind Speed</p>
+            <p className='detail-text'>Wind Speed</p>
           </div>
         </div>
       }
